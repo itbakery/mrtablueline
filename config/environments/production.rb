955f72b0d2156c9274692b7970617e3e64498c53
sawangpong muadphet <sawangpongm@gmail.com>
2012-09-15 19:46:51 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,13 +10,15 @@ Mrtablueline::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   #config.serve_static_assets = false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  config.assets.digest = true
+  config.assets.debug = true
   config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
