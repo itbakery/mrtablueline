a865a3ca819d1039958aeb5408720f63cc3b8700
sawangpong muadphet <sawangpongm@gmail.com>
2012-09-28 00:06:13 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Mrtablueline::Application.configure do
   config.assets.compile = true
   config.assets.digest = true
   config.assets.debug = true
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
