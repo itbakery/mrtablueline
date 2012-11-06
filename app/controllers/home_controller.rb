6e61baf1873756c9ef5003da7b8c4434ec5e91e8
lookmee <lookmee@itbfedora.local>
2012-11-06 14:46:30 +0700

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @pageroots = Page.roots.asc(:order)
     @activities = Activity.all.desc(:created_at).to_a
-    @pageactivities = Activity.all.desc(:created_at).page(params[:page]).per(5)
+    @pageactivities = Activity.all.page(params[:page]).desc(:created_at).per(5)
     @announces = Announce.search(params[:search])
     @announces10 = Announce.limit(10).to_a
     @traffics = Effect.effect_scope("Traffic").first.announces rescue nil
