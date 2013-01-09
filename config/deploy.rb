af5b832ebe1d04bb8a47c85fe8a6857457892f39
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-10 04:48:15 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,9 @@ namespace :deploy do
 #  end
   desc "install the necessary prerequisites"
   task :bundle_install, :roles => :app do
-    run "cd #{release_path} && bundle install"
+    #run "cd #{release_path} && bundle install"
+    #run "cd #{current_path} && LC_ALL='en_US.UTF-8' bundle install --deployment --without test"
+    run "cd #{current_path} && LC_ALL='en_US.UTF-8' bundle install"
     run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
     run "ln -nfs #{shared_path}/transport #{release_path}/public/transport"
   end
