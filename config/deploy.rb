9a09f0660e7d801eefca287e8ed14efaf5010d30
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-10 14:38:17 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :branch, "master"
 require 'rvm/capistrano'
 #set :rvm_type, :system
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-set :rvm_ruby_string, 'ruby-1.9.2-p194'
+set :rvm_ruby_string, 'ruby-1.9.3-p194'
 set :rvm_type, :user
 #before 'deploy', 'rvm:create_gemset'
 #set :rvm_bin_path, "/home/deploy/.rvm/bin"
