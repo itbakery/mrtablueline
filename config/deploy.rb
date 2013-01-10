f9b5da2026a5faa24324129f1464fd5dcd4bd104
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-10 14:32:25 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,8 @@ set :rvm_ruby_string, "1.9.3"
 set :rvm_type, :user
 before 'deploy', 'rvm:create_gemset'
 set :rvm_bin_path, "/home/deploy/.rvm/bin"
-set :rvm_ruby_string, "ruby-1.9.3-p194@mrtablueline"
+#set :rvm_ruby_string, "ruby-1.9.3-p194@mrtablueline"
+set :rvm_ruby_string, "ruby-1.9.3-p194"
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 #set :bundle_cmd , "/home/deploy/.rvm/gems/ruby-1.9.3-p194@mrtablueline/bin/bundle"
 
