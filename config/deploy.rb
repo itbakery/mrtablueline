868de1f1e1c9fb476dc6284f81ea2e913ac05676
sawangpong muadphet <sawangpongm@gmail.com>
2013-02-26 22:55:31 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,12 +17,12 @@ require 'rvm/capistrano'
 #set :rvm_type, :system
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 #set :rvm_ruby_string, 'ruby-1.9.3-p194@global'
-set :rvm_ruby_string, 'ruby-1.9.3'
+#set :rvm_ruby_string, 'ruby-1.9.3'
 set :rvm_type, :user
 set :rvm_bin_path, "/home/deploy/.rvm/bin"
 #before 'deploy', 'rvm:create_gemset'
 #set :rvm_ruby_string, "ruby-1.9.3-p194@mrtablueline"
-#set :rvm_ruby_string, "ruby-1.9.3-p194"
+set :rvm_ruby_string, "ruby-1.9.3-p392"
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 #set :bundle_cmd , "/home/deploy/.rvm/gems/ruby-1.9.3-p194@mrtablueline/bin/bundle"
 
