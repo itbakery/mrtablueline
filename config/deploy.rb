c0f9f906f101722203e987529a918f909475633f
sawangpong muadphet <sawangpongm@gmail.com>
2013-02-26 22:02:25 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,11 +38,16 @@ set :sidekiq_role, :sidekiq
 role :sidekiq, "203.146.127.169"
 
 #after "deploy:update_code", "deploy:bundle_install"
-after "deploy:update_code", "deploy:rvm:setup"
+#after "deploy:update_code", "deploy:rvm:setup"
 after :deploy, "deploy:rvm:trust_rvmrc"
+after :deploy, "deploy:bundle_install"
 after :deploy, "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
+  desc "install the necessary prerequisite"
+  task :bundle_install, :roles => :app do
+    run "cd #{current_path} && LC_ALL='en_US.UTF-8' bundle install"
+  end
   # desc "Skipping asset compilation with Capistrano"
   #  namespace :assets do
   #    task :precompile, :roles => :web, :except => { :no_release => true } do
