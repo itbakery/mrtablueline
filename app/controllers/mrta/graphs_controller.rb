02ad7602e86d6117cf04dae7d3290ed2e16b9e76
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-23 01:31:10 +0700

    commit

--- app/controllers/mrta/graphs_controller.rb
+++ app/controllers/mrta/graphs_controller.rb
@@ -1,7 +1,11 @@
 class Mrta::GraphsController < ApplicationController
   layout 'project'
   def index
-  	
+    if params[:id]
+        @project = Project.find(params[:id])
+    else
+        @project = Project.first
+    end
   end
 
 end
--- app/helpers/mrta/graphs_helper.rb
+++ app/helpers/mrta/graphs_helper.rb
@@ -1,2 +1,11 @@
 module Mrta::GraphsHelper
+    def tracks_chart_data
+        @tracks = @project.tracks.map do | track |
+            {
+                atmonth: track.atmonth,
+                projection: track.projection,
+                actual: track.actual
+            }
+        end
+    end
 end
