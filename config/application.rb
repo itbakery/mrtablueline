650386293d69604d3f9d3e5a1f2151761c848728
Sawangpong Muadphet <sawangpongm@gmail.com>
2013-08-26 09:43:51 +0700

    commit

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Mrtablueline
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
 
-    config.i18n.default_locale = :th
+    #config.i18n.default_locale = :th
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
