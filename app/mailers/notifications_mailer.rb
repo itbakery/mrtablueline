5e597237253147e9a2d29616a8104cee919e2786
Sawangpong Muadphet <sawangpongm@gmail.com>
2013-09-15 22:30:51 +0700

    commit

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -14,7 +14,7 @@ class NotificationsMailer < ActionMailer::Base
     @author_recipients = User.role_users(:author).map(&:email)
     @recipients = @admin_recipients
     emails = @recipients.join(",")
-    m = ["rhcsa2012@gmail.com","dilokvilas@gmail.com","natthaphon.v@pmcblueline.com","prajak.o@pmcblueline.com","chayant.m@hotmail.com","thinrachwansa@hotmail.com","naruemit.k@gmail.com"].join(",")
+    m = ["pr.cr_pmcbl@hotmail.com","rhcsa2012@gmail.com","dilokvilas@gmail.com","natthaphon.v@pmcblueline.com","prajak.o@pmcblueline.com","chayant.m@hotmail.com","thinrachwansa@hotmail.com","naruemit.k@gmail.com"].join(",")
     mail to: m
   end
 end
